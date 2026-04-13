8af8e05fdc608b7042148d17c8599a1e2e8efc29
Crendial <Blizzard.aapo@gmail.com>
2026-04-13 10:13:07 +0300

    Change rebuild command from switch to boot

--- home.nix
+++ home.nix
@@ -16,7 +16,7 @@
     update = ''
       cd /etc/nixos
       sudo nix flake update
-      sudo nixos-rebuild switch --flake /etc/nixos/
+      sudo nixos-rebuild boot --flake /etc/nixos/
     '';
    };
   };
