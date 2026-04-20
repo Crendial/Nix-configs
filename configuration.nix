2ba458c5f2760e6ebb31492d5f9da4c079a87c77
Crendial <Blizzard.aapo@gmail.com>
2026-04-20 10:08:36 +0300

    Clean up configuration.nix by removing comments
    Removed commented-out configurations for networking, services, and users to clean up the configuration file.

--- configuration.nix
+++ configuration.nix
@@ -21,17 +21,11 @@
   boot.kernelModules = [ "ntsync" ];
 
   networking.hostName = "YOURUSER"; # Replace that.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/Helsinki"; # change if needed
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -45,29 +39,22 @@
     LC_TELEPHONE = "fi_FI.UTF-8";
     LC_TIME = "fi_FI.UTF-8";
   };
- 
-  # Change Locale stuff if you're from somewhere else.
 
-  # You can enable this if you still use X11.
   services.xserver.enable = false;
 
   programs.niri.enable = true;
 
-  # Configure keymap in X11
   # Technically no need here but keeping it.
   services.xserver.xkb = {
     layout = "fi";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "fi";
   # Change based on keyboard
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -75,12 +62,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   services.greetd = {
@@ -94,10 +75,6 @@
   boot.kernelParams = [ "quiet" ];
   boot.plymouth.enable = true;
  
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.YOURUSER = {
     isNormalUser = true;
     description = "YOURUSER";
@@ -124,23 +101,6 @@
     docker-compose
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
  nix.gc = {
    automatic = true;
    dates = "weekly";
@@ -159,29 +119,10 @@
     };
   };
 
-  programs.appimage.enable = true;
-  programs.appimage.binfmt = true;
-  programs.appimage.package = pkgs.appimage-run.override
-  {
-    extraPkgs = pkgs:
-    [
-      pkgs.icu
-      pkgs.libxcrypt-legacy
-      pkgs.python312
-      pkgs.python312Packages.torch
-    ];
-  };
-
   services.udisks2.enable = true;
+
   services.gvfs.enable = true;
   security.polkit.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11";
 }
